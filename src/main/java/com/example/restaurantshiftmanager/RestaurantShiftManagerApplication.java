9841ff9a894dce147477e93417895d419c262181
mimic <miku.ngt.0928@gmail.com>
2026-06-27 18:43:40 +0900

    初期コミット

---
+++ src/main/java/com/example/restaurantshiftmanager/RestaurantShiftManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.restaurantshiftmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantShiftManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantShiftManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/restaurantshiftmanager/RestaurantShiftManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restaurantshiftmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantShiftManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
