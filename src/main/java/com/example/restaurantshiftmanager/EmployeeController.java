1a19f118d24c087d7b50fc5a68d0b8d8c057536e
mimic <miku.ngt.0928@gmail.com>
2026-06-27 19:24:56 +0900

    従業員一覧画面を追加

---
+++ src/main/java/com/example/restaurantshiftmanager/EmployeeController.java
@@ -0,0 +1,13 @@
+package com.example.restaurantshiftmanager;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class EmployeeController {
+
+    @GetMapping("/employees")
+    public String list() {
+        return "employees/list";
+    }
+}
